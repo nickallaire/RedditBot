4f8683c5a53b613cdd3d64cdc3c1546e9e4dc415
nickallaire <nick.allaire@gmail.com>
2017-08-15 16:21:31 -0700

    Added tutorial code, hope to modify soon

---
+++ reddit_bot/bot.py
@@ -0,0 +1,88 @@
+# A Reddit bot that posts ______ in comments
+# Created by Nicholas Allaire
+
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+import praw
+import time
+import re
+import requests
+import bs4
+
+path = '/Users/nsallaire/Documents/Developer/RedditBot/reddit_bot/commented.txt'
+# Location of file where id's of already visited comments are maintained
+
+header = 'This is a bot\n'
+footer = '\n\nGoodbye!\n'
+#Text to be posted
+
+def authenticate():
+    print('Authenticating...\n')
+    reddit = praw.Reddit('explainbot', user_agent = 'this_is_a_test')
+    print('Authenticated as {}\n'.format(reddit.user.me()))
+    return reddit
+
+
+def fetchdata(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    tag = soup.find('p')
+    data = ''
+    while True:
+        if isinstance(tag, bs4.element.Tag):
+            if (tag.name == 'h2'):
+                break
+            if (tag.name == 'h3'):
+                tag = tag.nextSibling
+            else:
+                data = data + '\n' + tag.text
+                tag = tag.nextSibling
+        else:
+            tag = tag.nextSibling
+    return data
+
+
+def run_bot(reddit):
+    for comment in reddit.subreddit('test').comments(limit = 250):
+        match = re.findall("[a-z]*[A-Z]*[0-9]*https://www.xkcd.com/[0-9]+", comment.body)
+        if match:
+            print("Link found in comment with comment ID: " + comment.id)
+            xkcd_url = match[0]
+            url_obj = urlparse(xkcd_url)
+            xkcd_id = int((url_obj.path.strip("/")))
+            myurl = 'http://www.explainxkcd.com/wiki/index.php/' + str(xkcd_id)
+
+            file_obj_r = open(path, 'r')
+
+            try:
+                explanation = fetchdata(myurl)
+            except:
+                print('Exception!!! Possibly incorrect xkcd URL...\n')
+                # Typical cause for this will be a URL for an xkcd that doesn not exist
+            else:
+                if comment.id not in file_obj_r.read().splitlines():
+                    print('Link is unique....posting explanation\n')
+                    comment.reply(header + explanation + footer)
+
+                    file_obj_r.close()
+
+                    file_obj_w = open(path, 'a+')
+                    file_obj_w.write(comment.id + '\n')
+                    file_obj_w.close()
+                else:
+                    print('Already visited link...no reply needed\n')
+
+            time.sleep(10)
+
+    print('Waiting 60 seconds...\n')
+    time.sleep(60)
+
+def main():
+    reddit = authenticate()
+    run_bot(reddit)
+
+
+if __name__ == '__main__':
+    main()
